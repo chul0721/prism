e0fee17fb483e3629b334ffbc201dffce68c8153
int <int@intui-iMac.local>
2021-03-03 12:15:11 +0900

    FUCKING ERROR

--- commands/embed.js
+++ commands/embed.js
@@ -8,7 +8,7 @@ module.exports = {
   usage: ",embed <공지 내용>",
   run: async (client, message, args) => {
     let inc = args[1].replace('<#','').replace('>','');
-    if (!message.member.roles.cache.include(["704003304992145441", "700654413857161266", "698618859480940604", "816462860879134731"])){
+    if (!message.member.roles.cache.has.include(["704003304992145441", "700654413857161266", "698618859480940604", "816462860879134731"])){
       return message.reply("관리자 권한이 없는 사람은 이 명령어를 실행할 수 없습니다.")
     }
     if (inc == null || undefined || "") return message.channel.send("공지를 보낼 채널을 `,채널설정 [채널ID]`를 통해 설정해주세요.")
