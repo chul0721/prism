272523556e528888ea7ff7571519899baf12ac42
int <int@intui-iMac.local>
2021-03-03 09:52:45 +0900

    test

--- commands/embed.js
+++ commands/embed.js
@@ -7,9 +7,8 @@ module.exports = {
     "임베드에 씌워진 공지를 보내요.",
   usage: ",embed <공지 내용>",
   run: async (client, message, args) => {
-    let inc = ops.noticeChannel;
-    console.log(ops)
-    if (!(message.member.roles.cache.has(816462860879134731))){
+    let inc = args[1].replace('<#','').replace('>','');
+    if (!(message.member.roles.cache.has("698618859480940604"))){
         return message.reply("관리자 권한이 없는 사람은 이 명령어를 실행할 수 없습니다.").then(m => m.delete(5000));
     }
     if (inc == null || undefined || "") return message.channel.send("공지를 보낼 채널을 `,채널설정 [채널ID]`를 통해 설정해주세요.")
